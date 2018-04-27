998b5c049251ac52559763f510d8642b838337e2
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-04-27 18:30:53 +0300

    finish layout

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import './main.scss';
 
+
+var hamburger = document.querySelector('.hamburger');
+var rightSidebar = document.querySelector('.right-sidebar');
+
+hamburger.addEventListener("click", function(e) {
+
+    rightSidebar.classList.toggle('is-hidden')
+
+});
